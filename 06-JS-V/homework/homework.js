88f2e063bd7db0727a44cbb80bfe626f01678ee9
galvanmg <68716898+galvanmg@users.noreply.github.com>
2020-07-31 10:58:43 -0300

    se agregan archivos nuevos

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,13 +9,31 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
 
+  function Usuario(opciones) {
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+
+  Usuario.prototype.saludar = function() {
+    return 'Hola, mi nombre es ' + this.nombre;
+  };
+
+  return Usuario;
+
 }
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
 
+
+  Constructor.prototype.saludar = function() {
+    return 'Hello World!';
+  };
 }
 
 function agregarStringInvertida() {
@@ -25,7 +43,16 @@ function agregarStringInvertida() {
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
 
-}
+  String.prototype.reverse = function() {
+    var inverter = "";
+    for (var i = this.length - 1; i>=0; i--){
+      inverter = inverter + this.charAt(i);
+    }
+    return inverter;
+    };
+
+  }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
